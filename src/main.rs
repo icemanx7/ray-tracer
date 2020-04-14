832898f759465378a3c375f45f7668b77201e32f
Richard Thomas <richard.thomas@interfront.co.za>
2020-04-14 17:23:03 +0200

    added simple image generation

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let image_width: i32 = 200;
+    let image_height: i32 = 200;
+
+    println!("{}", "P3");
+    println!("{} {}", image_width, image_height);
+    println!("{}", "255");
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r: f64 = (i as f64) / ( image_width as f64);
+            let g: f64 = (j as f64) / ( image_height  as f64) ;
+            let b: f64 = 0.2;
+
+            let ir: i64 = (255.998 * r) as i64;
+            let ig: i64 = (255.998 * g) as i64;
+            let ib: i64 = (255.998 * b) as i64;
+            println!("{} {} {}", ir, ig, ib)
+            }
+    }
+}
+
+
+fn test() {
+    
+}
