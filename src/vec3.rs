9dcaa6414d6d5acd9db27489714c3d568967ad44
Richard Thomas <richard.thomas@interfront.co.za>
2020-04-14 18:33:18 +0200

    added add assign and division assign

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, Mul, DivAssign, AddAssign, BitAnd, MulAssign};
 pub struct Vec3 {
    pub x: f64,
    pub y: f64,
@@ -29,5 +29,31 @@ impl Sub for Vec3 {
 
     fn sub(self, other: Vec3) -> Vec3 {
         Vec3 {x: self.x - other.x, y: self.y - other.y, z: self.z - other.z}
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, other: Vec3) -> Vec3 {
+        Vec3 {x: self.x * other.x, y: self.y * other.y, z: self.z * other.z}
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+
+    fn mul_assign(&mut self, rhs: f64) {
+       self.x *= rhs;
+       self.y *= rhs;
+       self.z *= rhs;
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+
+    fn div_assign(&mut self, rhs: f64){
+        self.x /= rhs;
+        self.y /= rhs;
+        self.z /= rhs;
     }
 }
