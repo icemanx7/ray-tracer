f0060dc7ff5e449fac3607f019f5c5bfd4e38fc6
Richard Thomas <richard.thomas@interfront.co.za>
2020-04-15 12:12:18 +0200

    added more traits and helper functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use vec3::{Vec3};
+use vec3::Vec3;
 mod vec3;
 
 fn main() {
@@ -9,18 +9,25 @@ fn main() {
     println!("{} {}", image_width, image_height);
     println!("{}", "255");
 
-    for j in 0..image_height {
+    for j in (0..image_height).rev() {
+        eprint!("{} {}", "\rlines remaining", j);
         for i in 0..image_width {
-            eprint!("{} {}" , "\rlines remaining" , j);
-            let r: f64 = (i as f64) / ( image_width as f64);
-            let g: f64 = (j as f64) / ( image_height  as f64) ;
-            let b: f64 = 0.2;
+            let vecTest: Vec3 = Vec3 {
+                x: (i as f64) / (image_width as f64),
+                y: (j as f64) / (image_height as f64),
+                z: 0.2,
+            };
 
-            let ir: i64 = (255.998 * r) as i64;
-            let ig: i64 = (255.998 * g) as i64;
-            let ib: i64 = (255.998 * b) as i64;
-            println!("{} {} {}", ir, ig, ib)
-            }
+            vecTest.write_color();
+            // let r: f64 = (i as f64) / (image_width as f64);
+            // let g: f64 = (j as f64) / (image_height as f64);
+            // let b: f64 = 0.2;
+
+            // let ir: i64 = (255.999 * r) as i64;
+            // let ig: i64 = (255.999 * g) as i64;
+            // let ib: i64 = (255.999 * b) as i64;
+            // println!("{} {} {}", ir, ig, ib)
+        }
     }
     println!("{}", "Done");
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,26 +1,58 @@
-use std::ops::{Add, Sub, Mul, DivAssign, AddAssign, BitAnd, MulAssign};
+use std::ops::{Add, AddAssign, DivAssign, Mul, MulAssign, Sub};
 pub struct Vec3 {
-   pub x: f64,
-   pub y: f64,
-   pub z: f64
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
-trait vec3H {
+// trait vec3H {
+//     fn x(&self) -> f64;
+//     fn y(&self) -> f64;
+//     fn z(&self) -> f64;
 
-    fn x(&self) -> f64;
-    fn y(&self) -> f64;
-    fn z(&self) -> f64;
+//     fn lenthSquared(self) -> f64;
+//     fn getLength(self) -> f64;
+//     fn write_color(self);
+// }
 
-    fn lenthSquared();
+impl Vec3 {
+    pub fn x(self) -> f64 {
+        self.x
+    }
 
+    pub fn y(&self) -> f64 {
+        self.y
+    }
 
+    pub fn z(&self) -> f64 {
+        self.z
+    }
+
+    pub fn lenthSquared(self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn getLength(self) -> f64 {
+        self.lenthSquared().sqrt()
+    }
+
+    pub fn write_color(self) {
+        let ir: i64 = (255.999 * self.x) as i64;
+        let ig: i64 = (255.999 * self.y) as i64;
+        let ib: i64 = (255.999 * self.z) as i64;
+        println!("{} {} {}", ir, ig, ib)
+    }
 }
 
 impl Add for Vec3 {
     type Output = Vec3;
 
     fn add(self, other: Vec3) -> Vec3 {
-        Vec3 {x: self.x + other.x, y: self.y + other.y, z: self.z + other.z}
+        Vec3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
     }
 }
 
@@ -28,7 +60,11 @@ impl Sub for Vec3 {
     type Output = Vec3;
 
     fn sub(self, other: Vec3) -> Vec3 {
-        Vec3 {x: self.x - other.x, y: self.y - other.y, z: self.z - other.z}
+        Vec3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
     }
 }
 
@@ -36,22 +72,32 @@ impl Mul for Vec3 {
     type Output = Vec3;
 
     fn mul(self, other: Vec3) -> Vec3 {
-        Vec3 {x: self.x * other.x, y: self.y * other.y, z: self.z * other.z}
+        Vec3 {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+impl AddAssign<Vec3> for Vec3 {
+    fn add_assign(&mut self, rhs: Vec3) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
     }
 }
 
 impl MulAssign<f64> for Vec3 {
-
     fn mul_assign(&mut self, rhs: f64) {
-       self.x *= rhs;
-       self.y *= rhs;
-       self.z *= rhs;
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
     }
 }
 
 impl DivAssign<f64> for Vec3 {
-
-    fn div_assign(&mut self, rhs: f64){
+    fn div_assign(&mut self, rhs: f64) {
         self.x /= rhs;
         self.y /= rhs;
         self.z /= rhs;
